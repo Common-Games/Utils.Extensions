79155d5de87411b5158bffdf48f450d9507419ff
Walter Hulsebos <walterhulsebos@outlook.com>
2021-08-03 12:25:23 +0200

    CHANGE: Interpolation.Int - Updated to Clean Primitive Types

--- Runtime/Math/Interpolation/Int.cs
+++ Runtime/Math/Interpolation/Int.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Runtime.CompilerServices;
 
 using JetBrains.Annotations;
@@ -6,18 +7,31 @@ using UnityEngine;
 
 namespace CGTK.Utilities.Extensions
 {
+	using F32 = Single;
+	using F64 = Double;
+
+	using I8  = SByte;
+	using I16 = Int16;
+	using I32 = Int32;
+	using I64 = Int64;
+
+	using U8  = Byte;
+	using U16 = UInt16;
+	using U32 = UInt32;
+	using U64 = UInt64;
+
 	namespace Math
 	{
 		public static partial class Interpolation
 		{
 			[PublicAPI]
 			[MethodImpl(MethodImplOptions.AggressiveInlining)]
-			public static int Lerp(this int from, in int to, in float t)
+			public static I32 Lerp(this I32 from, in I32 to, in F32 t)
 				=> Mathf.Lerp(a: from, b: to, t: t).RoundToInt();
 	
 			[PublicAPI]
 			[MethodImpl(MethodImplOptions.AggressiveInlining)]
-			public static int LerpUnclamped(this int from, in int to, in float t)
+			public static I32 LerpUnclamped(this I32 from, in I32 to, in F32 t)
 				=> Mathf.LerpUnclamped(a: from, b: to, t: t).RoundToInt();
 	
 			//[PublicAPI]
@@ -27,7 +41,7 @@ namespace CGTK.Utilities.Extensions
 			
 			[PublicAPI]
 			[MethodImpl(MethodImplOptions.AggressiveInlining)]
-			public static int LerpAngle(this int from, in int to, in float t)
+			public static I32 LerpAngle(this I32 from, in I32 to, in F32 t)
 				=> Mathf.LerpAngle(a: from, b: to, t: t).RoundToInt();
 		}
 	}
