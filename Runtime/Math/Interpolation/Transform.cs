410ac723a695d423afa6df15be2fa77bf0c3f883
Walter Hulsebos <walterhulsebos@outlook.com>
2021-08-03 12:27:12 +0200

    NEW: Interpolation.Transform (placeholder)

---
+++ Runtime/Math/Interpolation/Transform.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections;
+using System.Runtime.CompilerServices;
+
+using JetBrains.Annotations;
+
+using UnityEngine;
+
+namespace CGTK.Utilities.Extensions
+{
+	using F32 = Single;
+	using F64 = Double;
+
+	using I8  = SByte;
+	using I16 = Int16;
+	using I32 = Int32;
+	using I64 = Int64;
+
+	using U8  = Byte;
+	using U16 = UInt16;
+	using U32 = UInt32;
+	using U64 = UInt64;
+
+	namespace Math
+	{
+		public static partial class Interpolation
+		{
+			//[PublicAPI]
+			//[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			//public static IEnumerator LerpPosition(this Transform transform, in Vector3 to, in float t)
+			
+		}
+	}
+}
