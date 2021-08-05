c0295a54dbac1ee7a4acbc442339d9fb2647db12
Walter Hulsebos <walterhulsebos@outlook.com>
2021-08-05 18:22:27 +0200

    NEW: VectorExtensions.int4

---
+++ Runtime/Math/VectorExtensions/int4.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Runtime.CompilerServices;
+
+using JetBrains.Annotations;
+
+using Unity.Mathematics;
+
+namespace CGTK.Utilities.Extensions
+{
+	using F32 = Single;
+	using F64 = Double;
+
+	using I32 = Int32;
+	using I64 = Int64;
+
+	namespace Math
+	{
+		public static partial class VectorExtensions
+		{
+			[PublicAPI]
+			[MethodImpl(MethodImplOptions.AggressiveInlining)]
+			public static I32[] ToArray(in this int4 self)
+			{
+				I32[] __array = new I32[4];
+
+				__array[0] = self[0];
+				__array[1] = self[1];
+				__array[2] = self[2];
+				__array[3] = self[3];
+
+				return __array;
+			}
+
+		}
+	}
+}
